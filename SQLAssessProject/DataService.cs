adde00b0bcd5b94dac69c66bb5e1161df93ba514
ST4T1K-VOID <mjobrien06@hotmail.com>
2024-10-14 13:15:39 +0800

    Initial commit

---
+++ SQLAssessProject/DataService.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLproject
+{
+    class DataService
+    {
+        private List<Employee> employees = new List<Employee>();
+
+        public DataService()
+        {
+            AddEmployee(101, "Jane", "Bane", 100, 2);
+            AddEmployee(102, "Smelly", "SR", 100000000, 1);
+            AddEmployee(103, "Bob", "Builer", 70200, 1);
+            AddEmployee(104, "Poopy", "Joe", 1, 0);
+            AddEmployee(105, "Rae", "Summers", 56000, 3);
+        }
+
+        public enum updateFilter
+        {
+            employeeID = 0,
+            firstName = 1,
+            lastName = 2,
+            salary = 3,
+            branchID = 4,
+        }
+
+        updateFilter filter = new updateFilter();
+
+        public List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public List<Employee> FindEmployees()
+        {
+            return employees;
+        }
+
+        public void AddEmployee(int employeeID, string firstName, string lastName, int grossSalary, int branchID)
+        {
+            employees.Add(new Employee(employeeID, firstName, lastName, grossSalary, branchID));
+        }
+
+        public bool RemoveEmployee(Employee targetEmployee)
+        {
+            foreach (Employee employee in employees)
+            {
+                if (employee.ID == targetEmployee.ID)
+                {
+                    employees.Remove(employee);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //public void UpdateEmployee(updateFilter filter, Employee targetEmployee)
+        //{
+            
+
+
+
+
+        //    int castFilter = (int)filter;
+
+        //    switch (castFilter)
+        //    {
+        //        case 0:
+        //            var newInfo = 
+        //    }
+        //}
+
+        
+    }
+}
---
+++ SQLAssessProject/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLproject
+{
+    internal class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int GrossSalary { get; set; }
+        public int  BranchID { get; set; }
+
+        public Employee(int iD, string firstName, string lastName, int grossSalary, int branchID)
+        {
+            ID = iD;
+            FirstName = firstName;
+            LastName = lastName;
+            GrossSalary = grossSalary;
+            BranchID = branchID;
+        }
+    }
+}
